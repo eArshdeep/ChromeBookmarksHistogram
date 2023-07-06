b488c762defd51e249112b67c061c4929e9d92c8
Arshdeep Padda <aspadda@icloud.com>
2023-07-06 16:11:51 -0700

    Added root traversal
    Children folders are now explored as well.
    
    Performing recursive search on Bookmarks Bar and Other Bookmarks.

--- main.js
+++ main.js
@@ -5,10 +5,6 @@
  * This is the file that is in JSON format, not the HTML export you attain from within
  * the browser.
  * 
- * NOTES
- * 
- * - Currently only checks the "Other" bookmarks.
- * 
  * Arshdeep Padda
  * 5 July 2023
  * Node 18
@@ -22,15 +18,27 @@ let data = JSON.parse(fs.readFileSync("./Bookmarks"));
 let bookmarks = data["roots"]["other"];
 let histogram = new Map();
 
-// Populate Histogram
-bookmarks["children"].forEach(bookmark => {
-    if (bookmark.url == undefined) return;
+function traverse_root (root)
+{
+    root["children"].forEach(child => {
+        if (child.type == "url")
+        {
+            let url = URL.parse(child.url).hostname;
+            if (histogram.has(url)) histogram.set(url, histogram.get(url) + 1);
+            else histogram.set(url, 1);
+        }
 
-    let url = URL.parse(bookmark.url).hostname;
-    
-    if (histogram.has(url)) histogram.set(url, histogram.get(url) + 1);
-    else histogram.set(url, 1);
-});
+        if (child.type == "folder")
+        {
+            traverse_root(child);
+        }
+    });
+}
+
+traverse_root(data["roots"]["bookmark_bar"]);
+traverse_root(data["roots"]["other"]);
+
+histogram = new Map([...histogram.entries()].sort((a, b) => b[1] - a[1]));
 
 // Report Frequency
 let total = 0;
