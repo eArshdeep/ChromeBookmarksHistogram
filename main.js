df6dcb660c222841e1775c2c5b9c691ef40157e2
Arshdeep Padda <aspadda@icloud.com>
2023-07-06 16:49:26 -0700

    Added reporting of folder bookmark count

--- main.js
+++ main.js
@@ -17,15 +17,18 @@ var URL = require('url');
 let data = JSON.parse(fs.readFileSync("./Bookmarks"));
 let bookmarks = data["roots"]["other"];
 let histogram = new Map();
+let folderCount = new Map();
 
 function traverse_root (root)
 {
+    let count = 0;
     root["children"].forEach(child => {
         if (child.type == "url")
         {
             let url = URL.parse(child.url).hostname;
             if (histogram.has(url)) histogram.set(url, histogram.get(url) + 1);
             else histogram.set(url, 1);
+            count += 1;
         }
 
         if (child.type == "folder")
@@ -33,6 +36,7 @@ function traverse_root (root)
             traverse_root(child);
         }
     });
+    folderCount.set(root.name, count);
 }
 
 traverse_root(data["roots"]["bookmark_bar"]);
@@ -41,9 +45,15 @@ traverse_root(data["roots"]["other"]);
 histogram = new Map([...histogram.entries()].sort((a, b) => b[1] - a[1]));
 
 // Report Frequency
+console.log("Domain Frequency:")
 let total = 0;
 histogram.forEach((freq, url, map) => {
     console.log(url, freq);
     total += freq;
 });
 console.log("total:", total);
+
+console.log("\nFolders:");
+folderCount.forEach((count, folder, map) => {
+    console.log(folder, count);
+});
